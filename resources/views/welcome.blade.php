db1525a36fd6c8d4cf6f7d057b013d156176ae9b
AndrewArsenault <andrewarsenault12@hotmail.com>
2018-03-06 09:20:13 -0400

    changed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 - Andrew Sample App</div>
             </div>
         </div>
     </body>
